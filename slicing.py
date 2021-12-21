31f8f5a3ffd817101136b54e4326bfbe9dddeec9
Anna Toussaint <annatouss@gmail.com>
2021-12-21 16:06:41 -0400

    Added files

---
+++ slicing.py
@@ -0,0 +1,10 @@
+Python 2.7.18 (v2.7.18:8d21aa21f2, Apr 20 2020, 13:25:05) [MSC v.1500 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> s = "4lJ6E6aBTIlJmbrC967fCjYXZC71kBk20guJ6WaQadlLamprolepisMd11UKDzx8mpRlCXyMatDq7UMSySkb6svJi5AJM6BvHuITVbNmn3rtDWLyksTh7A72gWVsZvbeipQnJJfJjaeHVhSkjVBNUOydkbpwRfZFFVUfVcLqET84JwaUDJTPgmelonotiss3Y1WmW."
+>>> a = 43
+>>> b = 53
+>>> c = 181
+>>> d = 189
+>>> print s[a:b+1] + " " + s[c:d+1]
+Lamprolepis melonotis
+>>> 
