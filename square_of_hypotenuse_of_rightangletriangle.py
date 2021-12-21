93507cc9af8cde8b952c3b94fbf7822afd8dcabb
Anna <57022302+AnnaToussaint@users.noreply.github.com>
2021-12-21 17:24:04 -0400

    Delete square_of_hypotenuse_of_rightangletriangle.py

--- square_of_hypotenuse_of_rightangletriangle.py
+++
@@ -1,8 +0,0 @@
-Python 2.7.18 (v2.7.18:8d21aa21f2, Apr 20 2020, 13:25:05) [MSC v.1500 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
->>> a = 885
->>> b = 999
->>> c = a**2 + b**2
->>> print(c)
-1781226
->>> 
